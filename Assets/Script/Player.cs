93611f9d05bfcd0bd0ff2ffb63ea28c67966d4ed
Siksae <todn1225@gmail.com>
2023-06-14 01:55:19 +0900

    1일차 끝

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -9,26 +9,47 @@ public class Player : MonoBehaviour
     private BoxCollider2D m_2DBox;
     private Animator m_Anim;
 
-    private Vector3 m_moveDir;
-
+    [SerializeField] private Vector3 m_moveDir;
+    [SerializeField] private float m_playermovespeed = 5f;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        m_rigid = GetComponent<Rigidbody2D>();
+        m_2DBox = GetComponent<BoxCollider2D>();
+        m_Anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         moving();
+        checkAnim();
     }
-
+    
     private void moving()
     {
-            m_moveDir.x = Input.GetAxisRaw("Horizontal");
-            m_Anim.SetBool("Move", m_moveDir.x != 0);
-            
-
+        m_moveDir.x = Input.GetAxisRaw("Horizontal");
+        if (m_moveDir.x == 1f)
+        {
+            transform.position += Vector3.right * m_playermovespeed * Time.deltaTime;
+            transform.localScale = new Vector3(3f, 3f, 3f); 
         }
+        else if (m_moveDir.x == -1f)
+        {
+            transform.position += Vector3.left * m_playermovespeed * Time.deltaTime;
+            transform.localScale = new Vector3(-3f, 3f, 3f);
+        }
+        
+    }
+    private void checkAnim()
+    {
+        m_Anim.SetBool("move", m_moveDir.x != 0);
+        m_Anim.SetBool("attack", Input.GetKeyDown(KeyCode.Z));
+    }
+
+    private void Attack()
+    {
+        
     }
 }
